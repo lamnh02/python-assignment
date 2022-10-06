b521a760d2e3348f5347890390d3fcd0d3375325
Nguyen Huu Lam <huulam.nhl@gmail.com>
2022-10-06 15:27:06 +0700

    update_1.2

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import re
 
 
 def is_name_in_csv(word, csv_file):
-    with open(csv_file, "r") as f:
+    with open(csv_file, "r", encoding="utf8") as f:
         data = f.read()
     return bool(re.search(word, data))
 
 
 list_file_csv = ['content1.csv', 'content2.csv', 'content3.csv']
-word = "dụng"
+word = "email"
 for csv_file in list_file_csv:
     if is_name_in_csv(word, csv_file):
         print(f"""find "{word}" in {csv_file}""")
         print("\n")
-        with open(csv_file, "r") as f:
+        with open(csv_file, "r", encoding="utf8") as f:
             print(f.read())
