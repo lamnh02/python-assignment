8f0e12c1d6f92ad1a073486b022e70bcb53def82
Nguyen Huu Lam <huulam.nhl@gmail.com>
2022-10-07 11:46:56 +0700

    update_2

--- test.py
+++ test.py
@@ -1,18 +1,38 @@
-# import pandas as pd
-import re
-
-
-def is_name_in_csv(word, csv_file):
-    with open(csv_file, "r", encoding="utf8") as f:
-        data = f.read()
-    return bool(re.search(word, data))
-
 
 list_file_csv = ['content1.csv', 'content2.csv', 'content3.csv']
-word = "email"
+ 
+ 
+prior_dict = dict()
+for i in list_file_csv:
+    prior_dict[i] = 0.0
+ 
+ 
+key_word = input()
+key_word = key_word.lower()
+key_separate = key_word.split()
+ 
+ 
 for csv_file in list_file_csv:
-    if is_name_in_csv(word, csv_file):
-        print(f"""find "{word}" in {csv_file}""")
-        print("\n")
-        with open(csv_file, "r", encoding="utf8") as f:
-            print(f.read())
+    with open(csv_file, "r", encoding="utf8") as f:
+        data = f.read()
+        data = data.lower()
+        x = data.count(key_word)
+        if x >= 1:
+            prior_dict[csv_file] += (100 + (x-1)*10)
+        for key in key_separate:
+            y = data.count(key)
+            if y > x and x == 0:
+                prior_dict[csv_file] += (1 + (y-x-1)*0.5)
+            elif y > x and x != 0:
+                prior_dict[csv_file] += ((y-x)*0.5)
+ 
+ 
+prior_dict = dict(sorted(prior_dict.items(), key=lambda item: item[1], reverse = True))
+ 
+ 
+print("Ket qua search theo do uu tien:\n")
+ 
+ 
+for key in prior_dict:
+    if prior_dict[key] > 0:
+        print(key + "\n")
